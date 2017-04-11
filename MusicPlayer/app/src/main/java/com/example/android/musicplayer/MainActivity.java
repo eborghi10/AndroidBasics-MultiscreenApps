60a1654f9c63adb06040004a2bdd708fd78d4450
eborghi10 <emiliano_borghi@hotmail.com>
2017-04-11 09:04:46 -0300

    Fix MediaPlayer

--- MusicPlayer/app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -4,38 +4,49 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
-    private TextView playButton = (TextView) findViewById(R.id.playButton);
+    private Button playButton;
+    private Button stopButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.song);
-    }
+        playButton = (Button) findViewById(R.id.playButton);
+        stopButton = (Button) findViewById(R.id.stopButton);
 
-    public void play_cb(View view) {
+        mediaPlayer = MediaPlayer.create(this, R.raw.song_file);
 
-        if (!mediaPlayer.isPlaying()) {
-            mediaPlayer.start();
-            playButton.setText("PAUSE");
-            Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
-        } else {
-            mediaPlayer.pause();
-            playButton.setText("PLAY");
-            Toast.makeText(MainActivity.this, "Pause", Toast.LENGTH_SHORT).show();
-        }
-    }
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mediaPlayer.isPlaying()) {
+                    mediaPlayer.start();
+                    playButton.setText("PAUSE");
+                    Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
+                } else {
+                    mediaPlayer.pause();
+                    playButton.setText("PLAY");
+                    Toast.makeText(MainActivity.this, "Pause", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
-    public void stop_cb(View view) {
-        mediaPlayer.stop();
-        playButton.setText("-");
-        Toast.makeText(MainActivity.this, "Stop", Toast.LENGTH_SHORT).show();
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.stop();
+                playButton.setText("-");
+                Toast.makeText(MainActivity.this, "Stop", Toast.LENGTH_SHORT).show();
+                playButton.setClickable(false);
+            }
+        });
     }
 }
