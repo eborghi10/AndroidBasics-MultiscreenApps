8b02d8eaf89ef8ff26d938f9e16760eed1ca4db3
Emiliano Borghi <emiliano_borghi@hotmail.com>
2017-09-03 23:11:34 -0300

    Add onCompletion() to MediaPlayer

--- MiwokTranslate/app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ MiwokTranslate/app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,7 +1,6 @@
 package com.example.android.miwok;
 
 import android.app.Activity;
-import android.media.Image;
 import android.media.MediaPlayer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,7 +12,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
--- MusicPlayer/app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -5,48 +5,75 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MediaPlayer mediaPlayer;
-    private Button playButton;
-    private Button stopButton;
+    private MediaPlayer mMediaPlayer;
+    private Button mPlayButton;
+    private Button mStopButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        playButton = (Button) findViewById(R.id.playButton);
-        stopButton = (Button) findViewById(R.id.stopButton);
+        mPlayButton = (Button) findViewById(R.id.playButton);
+        mStopButton = (Button) findViewById(R.id.stopButton);
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.song_file);
+        mMediaPlayer = MediaPlayer.create(this, R.raw.song_file);
 
-        playButton.setOnClickListener(new View.OnClickListener() {
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!mediaPlayer.isPlaying()) {
-                    mediaPlayer.start();
-                    playButton.setText("PAUSE");
+                if (!mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.start();
+                    mPlayButton.setText(R.string.mp_pause);
                     Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
                 } else {
-                    mediaPlayer.pause();
-                    playButton.setText("PLAY");
+                    mMediaPlayer.pause();
+                    mPlayButton.setText(R.string.mp_play);
                     Toast.makeText(MainActivity.this, "Pause", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-        stopButton.setOnClickListener(new View.OnClickListener() {
+        mStopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mediaPlayer.stop();
-                playButton.setText("-");
+                mMediaPlayer.stop();
+                mPlayButton.setText("-");
                 Toast.makeText(MainActivity.this, "Stop", Toast.LENGTH_SHORT).show();
-                playButton.setClickable(false);
+                mPlayButton.setClickable(false);
             }
         });
+
+        // Must be called after the mMediaPlayer.start() method!!
+        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                releaseMediaPlayer();
+            }
+        });
+
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+
+            Toast.makeText(MainActivity.this, "I'm done", Toast.LENGTH_SHORT).show();
+        }
     }
 }
